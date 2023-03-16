d54cf41c6ff0b42f44b08eb242da4535a76cc87e
Giovanni Panza <panzagiovanni@gmail.com>
2023-03-16 13:44:30 +0100

    Update heal.py

--- CrazyFile/heal.py
+++ CrazyFile/heal.py
@@ -1,8 +1,8 @@
 import os
 
 # opening the file in read mode
-f1 = open("lista_oldfile.txt", "r")
-f2 = open("lista_newfile.txt", "r")
+f1 = open("list_oldfile.txt", "r")
+f2 = open("list_newfile.txt", "r")
 
 # reading the file
 data1 = f1.read()
@@ -14,11 +14,8 @@ data_into_list1 = data1.split("\n")
 data_into_list2 = data2.split("\n")
 
 for i in range(len(data_into_list1)):
-  print(data_into_list2[i])
-  print(data_into_list1[i])
   os.rename(data_into_list2[i], data_into_list1[i])
 
 
-print(data_into_list1)
 f1.close()
 f2.close()
